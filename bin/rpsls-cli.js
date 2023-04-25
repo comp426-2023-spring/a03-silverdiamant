cf0928bc406523c13573434a11c8c5577cacc9b2
Annie Vo <avo2@unc.edu>
2023-04-24 23:46:04 -0400

    fix

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -48,19 +48,6 @@ if (args._.length = 0) {
 if (result == undefined) {
         console.error('Out of range');
         console.log(
-                `Usage: node-rpsls [SHOT]
-                Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-    
-                -h, --help        display this help message and exit
-                -r, --rules       display the rules and exit
-    
-                Examples:
-                node-rpsls        Return JSON with single player RPSLS result.
-                                    e.g. {"player":"rock"}
-                node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
-    
-        console.log(
             `Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
     
             - Scissors CUTS Paper
