f7ff3a8a868e57a48abbaedf5b96507bd5bbc3d6
Annie Vo <avo2@unc.edu>
2023-04-24 22:19:34 -0400

    fixing stringify

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -5,45 +5,7 @@ import { rps } from "../lib/rpsls.js";
 
 const args = minimist(process.argv.slice(2));
 
-let shot = args._[0];
-
-// if no argument supplied
-
-if (args._.length = 0) {
-        console.log(JSON.stringify(rps()));
-        process.exit(0);
-}
-
-// if unlabeled argument supplied
 let result = rps(args._[0]);
-if (result == undefined) {
-        console.error('Out of range');
-        console.log(
-                `Usage: node-rps [SHOT]
-                Play Rock Paper Scissors (RPS)
-
-                -h, --bruh      display this help message and exit
-                -r, --rules     display the rules and exit
-
-                Examples:
-                node-rps        Return JSON with single player RPS result.
-                                e.g. {"player":"rock"}
-                node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                                e.g {"player":"rock","opponent":"scissors","result":"win"}`);
-
-        console.log(
-                `Rules for Rock Paper Scissors:
-
-                - Scissors CUTS Paper
-                - Paper COVERS Rock
-                - Rock CRUSHES Scissors`);
-        process.exit(0);
-} else {
-        console.log(JSON.stringify(result));
-        process.exit(0);
-}
-
-// help message
 
 if (args.h || args.help) {
         console.log(
@@ -61,14 +23,44 @@ if (args.h || args.help) {
         process.exit(0);
 }
 
-// rules message
-
 if (args.r || args.rules) {
         console.log(
                 `Rules for Rock Paper Scissors:
                 - Scissors CUTS Paper
                 - Paper COVERS Rock
                 - Rock CRUSHES Scissors`);
+        process.exit(0);
+}
+
+if (args._.length = 0) {
+        console.log(JSON.stringify(rps(args._[0])));
+        process.exit(0);
+}
+
+if (result == undefined) {
+        console.error('Out of range');
+        console.log(
+                `Usage: node-rps [SHOT]
+                Play Rock Paper Scissors (RPS)
+
+                -h, --help      display this help message and exit
+                -r, --rules     display the rules and exit
+
+                Examples:
+                node-rps        Return JSON with single player RPS result.
+                                e.g. {"player":"rock"}
+                node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                                e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+
+        console.log(
+                `Rules for Rock Paper Scissors:
+
+                - Scissors CUTS Paper
+                - Paper COVERS Rock
+                - Rock CRUSHES Scissors`);
+        process.exit(0);
+} else {
+        console.log(JSON.stringify(result));
         process.exit(0);
 }
 
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,20 +1,50 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist';
-import { rpsls } from "/lib/rpsls.js";
+import { rpsls } from "../lib/rpsls.js";
 
 const args = minimist(process.argv.slice(2));
 
-// if no argument supplied
+let result = rpsls(args._[0]);
 
-if (args._.length = 0) {
-        console.log(JSON.stringify(rpsls()));
+if (args.h || args.help) {
+        console.log(
+            `Usage: node-rpsls [SHOT]
+            Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+
+            -h, --help        display this help message and exit
+            -r, --rules       display the rules and exit
+
+            Examples:
+            node-rpsls        Return JSON with single player RPSLS result.
+                                e.g. {"player":"rock"}
+            node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                                e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
         process.exit(0);
 }
 
-// if unlabeled argument supplied
+if (args.r || args.rules) {
+        console.log(
+            `Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
 
-let result = rpsls(args._[0]);
+                - Scissors CUTS Paper
+                - Paper COVERS Rock
+                - Rock SMOOSHES Lizard
+                - Lizard POISONS Spock
+                - Spock SMASHES Scissors
+                - Scissors DECAPITATES Lizard
+                - Lizard EATS Paper
+                - Paper DISPROVES Spock
+                - Spock VAPORIZES Rock
+                - Rock CRUSHES Scissors`);
+        process.exit(0);
+}
+
+if (args._.length = 0) {
+        console.log(JSON.stringify(rpsls(args._[0])));
+        process.exit(0);
+}
+
 if (result == undefined) {
         console.error('Out of range');
         console.log(
@@ -48,42 +78,6 @@ if (result == undefined) {
         process.exit(0);
 } else {
         console.log(JSON.stringify(result));
-        process.exit(0);
-}
-
-// help message
-
-if (args.h || args.help) {
-        console.log(
-            `Usage: node-rpsls [SHOT]
-            Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-
-            -h, --help        display this help message and exit
-            -r, --rules       display the rules and exit
-
-            Examples:
-            node-rpsls        Return JSON with single player RPSLS result.
-                                e.g. {"player":"rock"}
-            node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                                e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
-        process.exit(0);
-}
-// rules message
-
-if (args.r || args.rules) {
-        console.log(
-            `Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
-
-                - Scissors CUTS Paper
-                - Paper COVERS Rock
-                - Rock SMOOSHES Lizard
-                - Lizard POISONS Spock
-                - Spock SMASHES Scissors
-                - Scissors DECAPITATES Lizard
-                - Lizard EATS Paper
-                - Paper DISPROVES Spock
-                - Spock VAPORIZES Rock
-                - Rock CRUSHES Scissors`);
         process.exit(0);
 }
 
